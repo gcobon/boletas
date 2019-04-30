1699177ea7ba81d611a3c5dc06b71bfa3b508dea
gcobon <gudielcobon@gmail.com>
2019-04-29 20:29:14 -0600

    add libraries

---
+++ boletas/src/main/Boletas.java
@@ -0,0 +1,21 @@
+
+package main;
+
+import modelo.conexionDB;
+
+/**
+ *
+ * @author gudiel
+ */
+public class Boletas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        conexionDB con = new conexionDB();
+        con.conectar();
+       
+    }
+    
+}
